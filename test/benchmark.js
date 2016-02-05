51745dcc6384f70dda46c6bff3032bc7cf650960
Dmitry V. Romanov <fortsq@gmail.com>
2016-02-06 01:03:22 +0300

    Test benchmark

---
+++ test/benchmark.js
@@ -0,0 +1,52 @@
+// > npm run benchmark
+
+'use strict';
+
+var container = require('../container');
+var Service = require('./service');
+
+const COUNT = 1000000; // алых роз
+
+runBenchmark('Memory used with lazy loading', (i) => {
+    container['init_on_require_' + i] = container.share(function () { return new Service(); });
+});
+
+runBenchmark('Memory used without lazy loading', (i) => {
+    container['init_on_start_' + i] = new Service();
+});
+
+runBenchmark('Memory used without lazy loading (retry)', (i) => {
+    container['init_on_start_retry_' + i] = new Service();
+});
+
+runBenchmark('Memory used with lazy loading (retry)', (i) => {
+    container['init_on_require_retry_' + i] = container.share(function () { return new Service(); });
+});
+
+/**
+ * Запуск теста производительности
+ *
+ * @param text      Название
+ * @param callback  Что делаем внутри
+ */
+function runBenchmark(text, callback) {
+    gc(); // очищаем сборщик мусора (глобальная функция)
+    var time = process.hrtime();
+    var initialMegabyte = getMemoryMegabyteUse();
+    for (let i = 0; i < COUNT; i++) {
+        callback(i); // сетим свойство объекта
+    }
+    var finalMegabyte = getMemoryMegabyteUse();
+    var runTime = process.hrtime(time); // сразу вычисляет разницу времени, возвращает массив [секунды, наносекунды]
+    console.log(text + ': %d MB - %d.%d s', (finalMegabyte - initialMegabyte), runTime[0], (runTime[1] / 1000000).toFixed());
+}
+
+/**
+ * Кол-во МБ используемой памяти кучи
+ *
+ * @returns {string}
+ */
+function getMemoryMegabyteUse() {
+    var state = process.memoryUsage();
+    return (state.heapUsed / 1024 / 1024).toFixed();
+}
